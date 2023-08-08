73e7cd2f828c7239e30ef87dc5d43f33d5dc6a7d
Bcypher01 <olumidebabayemi1@gmail.com>
2023-08-08 12:19:28 +0100

    finish up blog ui

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 const BlogPost = () => {
-  return <div>BlogPost</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Desc</p>
+          <div className={styles.author}>
+            <Image
+              src=""
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>User</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className="">
+        <p className={styles.text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          reiciendis deleniti blanditiis, neque odit magnam iusto omnis.
+          Nesciunt laboriosam commodi earum dolore. A ex esse facilis voluptas
+          illo aliquid quam?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,41 @@
-import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 const Blog = () => {
   return (
-    <div className={styles.container}>
-      <Link href="" className={styles.container}></Link>
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
     </div>
   );
 };
