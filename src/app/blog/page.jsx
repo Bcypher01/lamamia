87be78455d8acd9b8a275bab9bb75dae930b0dc3
Bcypher01 <olumidebabayemi1@gmail.com>
2023-08-08 10:14:57 +0100

    finish up portfolio ui

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className={styles.container}>
+      <Link href="" className={styles.container}></Link>
+    </div>
+  );
 };
 
 export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import styles from "./page.module.css";
+import Button from "../../../../components/Button/Button";
+import Image from "next/image";
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1>Test</h1>
+          <p>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.unsplash.com/photo-1559028012-481c04fa702d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=736&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1>Test</h1>
+          <p>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.unsplash.com/photo-1559028012-481c04fa702d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=736&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
