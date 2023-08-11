4347db29c77ec1c89cce83eb98dad829522adf1e
Bcypher01 <olumidebabayemi1@gmail.com>
2023-08-11 09:04:44 +0100

    client side data fetching

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,33 @@
+"use client";
 import React from "react";
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        // next: { revalidate: 10 },
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await res.json();
+
+      setData(data);
+      setIsLoading(false);
+    };
+
+    getData();
+  }, []);
+  console.log(data);
   return <div>Dashboard</div>;
 };
 
